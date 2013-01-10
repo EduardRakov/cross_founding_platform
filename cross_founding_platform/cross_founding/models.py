82a21e0fb1072141c0e5c1ff68309d97f59a0d75
Eddie Rakov <eduardr@softwareplanet.uk.com>
2013-01-10 14:50:08 +0200

    'changed model'

--- cross_founding_platform/cross_founding/models.py
+++ cross_founding_platform/cross_founding/models.py
@@ -11,16 +11,15 @@ class Backer(models.Model):
     pass
 
 class User(models.Model):
-    user_name = models.CharField(max_length=20)
+    user_name = models.CharField(max_length=20, unique=True)
     full_name = models.CharField(max_length=50)
-    e_mail = models.EmailField()
-    password = models.CharField(max_length=25)
+    e_mail = models.EmailField(unique=True)
+    password = models.CharField(max_length=20)
     gender = models.CharField(max_length=10)
     date_of_birth = models.DateField()
     location = models.CharField(max_length=20)
     project_owner = models.ForeignKey(Project)
     backer = models.ForeignKey(Backer)
-
 
     def __unicode__(self):
         return self.user_name
