76927ea5e6b1b20590815268265fcf39cb0d9d63
Eddie Rakov <eduardr@softwareplanet.uk.com>
2013-01-17 09:49:03 +0200

    "django-registration, regbackend"

--- cross_founding_platform/cross_founding/views.py
+++ cross_founding_platform/cross_founding/views.py
@@ -1,9 +1,5 @@
 from django.contrib.auth.models import User
-from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponseRedirect
-from django.shortcuts import render_to_response, get_object_or_404
-#from cross_founding_platform.cross_founding.backer_form import BackerForm
-from cross_founding_platform.cross_founding.models import Backer
+from django.shortcuts import render_to_response
 
 def profile(request):
     backer = User.objects.all()
--- cross_founding_platform/urls.py
+++ cross_founding_platform/urls.py
@@ -8,10 +8,7 @@ from cross_founding_platform.cross_founding.forms import BackerRegistrationForm
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'cross_founding_platform.views.home', name='home'),
-    # url(r'^cross_founding_platform/', include('cross_founding_platform.foo.urls')),
-#    (r'^accounts/', include('registration.urls')),
+
 #    (r'^accounts/', include('registration.backends.default.urls')),
 
 #    (r'^accounts/register/$', 'registration.views.register', {'form_class':BackerRegistrationForm}),
@@ -22,17 +19,6 @@ urlpatterns = patterns('',
         name='registration_register'),
 
     url(r'^accounts/',  include(regUrls)),
-#    url(r'^accounts/activate/complete/$', 'gamb.profile.views.redirect_after_activation'),
-
-#    url(r'^accounts/register/$',
-#        register,
-#        {'backend': 'accounts.regbackend.RegBackend','form_class':BackerRegistrationForm},
-#        name='registration_register'
-#    ),
-
     url(r'^accounts/profile', 'cross_founding_platform.cross_founding.views.profile'),
-#    url(r'^cfp/sign_up_form', 'cross_founding_platform.cross_founding.views.sign_up_form'),
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
