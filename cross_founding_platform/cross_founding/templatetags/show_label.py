6da9b30651f9434b6abb17049b23d7c9832cc0bc
DmitryG <dmitryg@softwareplanet.uk.com>
2013-01-29 11:58:23 +0200

    show_label filter

--- cross_founding_platform/cross_founding/templatetags/show_label.py
+++ cross_founding_platform/cross_founding/templatetags/show_label.py
@@ -3,6 +3,6 @@ from django import template
 register = template.Library()
 
 @register.filter(name='show_label')
-def show_label(BackerRegistrationForm, arg):
-    return arg
+def show_label(BackerRegistrationForm, args):
+    return args
 
