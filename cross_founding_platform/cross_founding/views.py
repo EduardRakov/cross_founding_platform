1e95ed4bfa6c675c7867d9b441d2e53816f58bbd
DmitryG <dmitryg@softwareplanet.uk.com>
2013-01-29 18:05:10 +0200

    view, login, registration_form changed commit

--- cross_founding_platform/cross_founding/views.py
+++ cross_founding_platform/cross_founding/views.py
@@ -1,41 +1,67 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.sites.models import get_current_site
 from django.http import HttpResponseRedirect
-from django.views.decorators.csrf import csrf_protect
 from django.shortcuts import render_to_response
+from django.contrib.auth import REDIRECT_FIELD_NAME, login as auth_login, logout as auth_logout
 from django.template import RequestContext
+from django.contrib.auth import login
+#from this import c
+from django.template.response import TemplateResponse
+from django.utils.datastructures import MultiValueDictKeyError
+from django.utils.http import is_safe_url
+from django.views.decorators.cache import never_cache
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.debug import sensitive_post_parameters
+from cross_founding_platform import settings
 
-from cross_founding_platform.cross_founding.forms import BackerRegistrationForm
-from cross_founding_platform.cross_founding.models import Backer
 
 def profile(request):
     return render_to_response("profile.html")
 
-#@csrf_protect
-#def backer_registration(request, *args, **kwargs):
-#
-#    if request.method == "POST":
-#        form = BackerRegistrationForm(request.POST)
-#
-#        if form.is_valid():
-#            user = User()
-#            user.username = form.cleaned_data['username']
-#            user.email = form.cleaned_data['email']
-#            user.password = form.cleaned_data['password1']
-#            user.first_name = form.cleaned_data["first_name"]
-#            user.last_name = form.cleaned_data["last_name"]
-#            user.save()
-#
-#            backer = Backer(user=user)
-#            backer.gender = form.cleaned_data["gender"]
-#            backer.location = request.META['REMOTE_ADDR']
-#            backer.dob_at = form.cleaned_data['dob_date']
-#            backer.save()
-#
-#            return HttpResponseRedirect('/accounts/register/complete/')
-#    else:
-#        form = BackerRegistrationForm()
-#
-#    context = {'form': form}
-#
-#    return render_to_response('registration/registration_form.html', context,
-#        context_instance=RequestContext(request))
+@sensitive_post_parameters()
+@never_cache
+@csrf_protect
+def login(request, template_name='registration/login.html',
+          redirect_field_name=REDIRECT_FIELD_NAME,
+          authentication_form=AuthenticationForm,
+          current_app=None, extra_context=None):
+
+    redirect_to = request.REQUEST.get(redirect_field_name, '')
+
+    if request.method == "POST":
+        form = authentication_form(data=request.POST)
+#        try:
+#            if form.data['stay_signed_in']:
+#                print request.session.test_cookie_worked()
+#                pass
+#        except MultiValueDictKeyError:
+#            SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+
+        if form.is_valid():
+            if not is_safe_url(url=redirect_to, host=request.get_host()):
+                redirect_to = settings.LOGIN_REDIRECT_URL
+
+            auth_login(request, form.get_user())
+
+            if request.session.test_cookie_worked():
+                request.session.delete_test_cookie()
+
+            return HttpResponseRedirect(redirect_to)
+    else:
+        form = authentication_form(request)
+
+
+    request.session.set_test_cookie()
+
+    current_site = get_current_site(request)
+
+    context = {
+        'form': form,
+        redirect_field_name: redirect_to,
+        'site': current_site,
+        'site_name': current_site.name,
+        }
+    if extra_context is not None:
+        context.update(extra_context)
+    return TemplateResponse(request, template_name, context,
+        current_app=current_app)
