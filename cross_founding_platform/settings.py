9570d9f1c171fd8f7cb473d5a405f474937b54a8
eduardr <eduardr@softwareplanet.uk.com>
2013-02-07 17:27:31 +0200

    "chg settings.py"

--- cross_founding_platform/settings.py
+++ cross_founding_platform/settings.py
@@ -9,25 +9,14 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-#PASSWORD_HASHERS = (
-#    'django.contrib.auth.hashers.PBKDF2PasswordHasher',
-#    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
-#    'django.contrib.auth.hashers.BCryptPasswordHasher',
-#    'django.contrib.auth.hashers.SHA1PasswordHasher',
-#    'django.contrib.auth.hashers.MD5PasswordHasher',
-#    'django.contrib.auth.hashers.CryptPasswordHasher',
-##    'root',
-#    )
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'cfpdb',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'root',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'NAME': 'cfpdb', # Or path to database file if using sqlite3.
+        'USER': 'root', # Not used with sqlite3.
+        'PASSWORD': 'root', # Not used with sqlite3.
+        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '', # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -45,23 +34,26 @@ USE_L10N = True
 USE_TZ = True
 
 
-MEDIA_ROOT = '/home/dmitryg/PycharmProjects/cross_founding_platform/templates'
+MEDIA_ROOT = '/home/eduardr/PycharmProjects/cross_founding_platform/templates'
 
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = '/home/dmitryg/PycharmProjects/cross_founding_platform/cross_founding_platform/cross_founding/templates/'
+STATIC_ROOT = '/home/eduardr/PycharmProjects/cross_founding_platform/cross_founding_platform/cross_founding/static_collected'
 
 STATIC_URL = '/static/'
 
+LOGIN_REDIRECT_URL = '/accounts/profile'
+
 STATICFILES_DIRS = (
-    '/home/dmitryg/PycharmProjects/cross_founding_platform/templates',
-)
+    '/home/eduardr/PycharmProjects/cross_founding_platform/templates',
+    )
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
     )
+
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = 'gs-lxj*lkl1qvkcgz+ao(hyyp&amp;&amp;0@l2qhep0sni%t!b(_0k2jk'
@@ -70,12 +62,8 @@ SECRET_KEY = 'gs-lxj*lkl1qvkcgz+ao(hyyp&amp;&amp;0@l2qhep0sni%t!b(_0k2jk'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-    #     'django.template.loaders.eggs.Loader',
+    # 'django.template.loaders.eggs.Loader',
     )
-
-LOGIN_REDIRECT_URL = '/accounts/profile'
-
-SESSION_EXPIRE_AT_BROWSER_CLOSE = False
 
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
@@ -83,6 +71,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+#    'auth_remember.middleware.AuthRememberMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     )
@@ -92,37 +81,7 @@ ROOT_URLCONF = 'cross_founding_platform.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'cross_founding_platform.wsgi.application'
 
-TEMPLATE_DIRS = ('/home/dmitryg/PycharmProjects/cross_founding_platform/templates',)
-#
-#TEMPLATE_CONTEXT_PROCESSORS = (
-#    "django.core.context_processors.request",
-#    "allauth.account.context_processors.account",
-#    "allauth.socialaccount.context_processors.socialaccount",
-#    )
-
-AUTHENTICATION_BACKENDS = (
-    # Needed to login by username in Django admin, regardless of `allauth`
-    "django.contrib.auth.backends.ModelBackend",
-
-    # `allauth` specific authentication methods, such as login by e-mail
-    "allauth.account.auth_backends.AuthenticationBackend",
-
-    )
-
-AUTH_USER_EMAIL_UNIQUE = True
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
-EMAIL_USE_TLS = False
-
-#BANDIT_EMAIL = 'bandit@example.com'
-DEFAULT_FROM_EMAIL = 'info@google.ru'
-#EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'  #email in file
-#EMAIL_FILE_PATH = '/home/dmitryg/test_email_folder/'
-
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #email in console
-#EMAIL_BACKEND = 'bandit.backends.smtp.HijackSMTPBackend' #bandit email
+TEMPLATE_DIRS = ('/home/eduardr/PycharmProjects/cross_founding_platform/templates',)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
@@ -131,31 +90,40 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
-    'south',
+    #'django.contrib.admindocs',
+    #'south',
+#    'openid',
+    'oauth2',
     'registration',
     'django_nose',
-    'cross_founding_platform.cross_founding.templatetags',
-    'bandit',
-    'selenium',
-    'splinter',
-#    'allauth',
-#    'allauth.account',
-#    'allauth.socialaccount',
-#    'allauth.socialaccount.providers.facebook',
+#    'auth_remember',
     'cross_founding_platform.cross_founding',
-    )
+)
 
-SOCIALACCOUNT_PROVIDERS =\
-{ 'facebook':
-      { 'SCOPE': ['email', 'publish_stream'],
-        'METHOD': 'oauth2' ,
-        'LOCALE_FUNC': 'path.to.callable'} }
+AUTH_USER_EMAIL_UNIQUE = True
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 1025
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = 'info@example.com'
 
-AUTH_PROFILE_MODULE = 'cross_founding.model.backer'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+FACEBOOK_OAUTH_DIALOG_URL = "https://www.facebook.com/dialog/oauth?"
+FACEBOOK_ACCESS_TOKEN_URL = 'https://graph.facebook.com/oauth/access_token?'
+FACEBOOK_GRAPH_API_URL = 'https://graph.facebook.com/me?access_token='
+FACEBOOK_REDIRECT_URI = 'http://127.0.0.1:8000/facebook_register/'
+FACEBOOK_APP_ID = '467506353311178'
+FACEBOOK_APP_SECRET = 'd5f87532dc5623a3373453c61ba3f0c9'
+
+TWITTER_REQUEST_TOKEN_URL = 'https://api.twitter.com/oauth/request_token'
+TWITTER_ACCESS_TOKEN_URL  = 'https://api.twitter.com/oauth/access_token'
+TWITTER_AUTHORIZATION_URL = 'https://api.twitter.com/oauth/authorize'
+TWITTER_AUTHENTICATE_URL = 'https://api.twitter.com/oauth/authenticate?oauth_token='
+TWITTER_OAUTH_CONSUMER_KEY = "SiAxtvUQc6Z6bVR2Vi0A"
+TWITTER_OAUTH_CONSUMER_SECRET_KEY = "sQgtyyO7siQyqEx6609ZfC052lOzsmGSOOh9VG0yvuk"
 
 ACCOUNT_ACTIVATION_DAYS = 2
 
